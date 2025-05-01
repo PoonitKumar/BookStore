0c22992ac248733a125233fb8d0beed0fba02c22
Poonit Kumar <poonit1000@gmail.com>
2025-05-01 16:14:58 +0530

    first-commit

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = process.env.PORT || 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://Poonit1000:Raddop%40235@book-store.yrpmpnq.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store";
