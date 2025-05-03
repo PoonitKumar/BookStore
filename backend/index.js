372109dee858a3236dd110ba8f5d5d92f3115b74
Poonit Kumar <poonit1000@gmail.com>
2025-05-03 15:53:01 +0530

    cors origin updated

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,10 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://book-store-frontend-ujuj.onrender.com",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
   })
