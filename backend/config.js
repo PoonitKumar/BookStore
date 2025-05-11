922ccc5eff748bb0d99619e61d0f2712193ba529
Poonit Kumar <poonit1000@gmail.com>
2025-05-11 15:39:39 +0530

    Remove exposed config.js containing MongoDB URI

--- backend/config.js
+++
@@ -1,4 +0,0 @@
-export const PORT = process.env.PORT || 5555;
-
-export const mongoDBURL =
-  "mongodb+srv://Poonit1000:Raddop%40235@cluster0.1q4wbeu.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0";
