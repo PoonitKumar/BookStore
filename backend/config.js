cba5f06abefddd6853077eb43461d30fde79e891
Poonit Kumar <poonit1000@gmail.com>
2025-05-11 19:22:18 +0530

    Remove config file with sensitive info

--- backend/config.js
+++
@@ -1,4 +0,0 @@
-export const PORT = process.env.PORT || 5555;
-
-export const mongoDBURL =
-  "mongodb+srv://bookstoreUser:WwpX5kJclOnWpYrR@cluster0.1q4wbeu.mongodb.net/bookstore?retryWrites=true&w=majority&appName=Cluster0";
